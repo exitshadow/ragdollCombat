5eff6f658722c19a3a5654488d26ae144db82d0a
exitshadow <exertingshadows@gmail.com>
2022-07-30 18:32:03 +0200

    fluider movements, hit force multiplies with holding button time

--- Assets/_Scripts/HitOnClick.cs
+++ Assets/_Scripts/HitOnClick.cs
@@ -10,20 +10,32 @@ public class HitOnClick : MonoBehaviour
     private RaycastHit hit;
     private Ray ray;
 
+    private float timeClickDown;
+    private float timeClickUp;
+    private float timeClickHold;
+    
     
 
     void Update()
     {
+
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+        if(Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
         {
+            timeClickDown = Time.fixedTime;
+        }
+
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonUp(0))
+        {
+            timeClickUp = Time.fixedTime;
+            timeClickHold = timeClickUp - timeClickDown + 1f;
             if (hit.collider.GetComponent<Rigidbody>() != null)
             {
                 Vector3 incomingVec = hit.point - hitOrigin.position;
                 Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
 
-                hit.collider.GetComponent<Rigidbody>().AddForce(reflectVec * strength * -1, ForceMode.Impulse);
+                hit.collider.GetComponent<Rigidbody>().AddForce(reflectVec * strength * -1 * timeClickHold, ForceMode.Impulse);
             }
         }
     }
--- Assets/_Scripts/LookAtTarget.cs
+++ Assets/_Scripts/LookAtTarget.cs
@@ -10,6 +10,9 @@ public class LookAtTarget : MonoBehaviour
     {
         Vector3 targetPos = target.transform.position;
         transform.LookAt(targetPos, Vector3.up);
+        //Quaternion targetRot = Quaternion.LookRotation(targetPos);
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime);
 
     }
 }
